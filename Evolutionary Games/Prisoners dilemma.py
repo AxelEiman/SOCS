70b4faf7f792b7c5db25a9a9a1c7ab88445c510c
AxelEiman <axeleiman@gmail.com>
2021-12-01 16:50:46 +0100

    Start

---
+++ Evolutionary Games/Prisoners dilemma.py
@@ -0,0 +1,13 @@
+
+
+N = 10      # Number of rounds
+T = 0       # Punishment for one snitch
+R = 0.5     # Punishment if neither snitch
+P = 1       # Punishment if both snitch
+S = 1.5     # Punishment if snitched on
+
+# Exercise 13.1
+m = 6       # Opponents strategy
+
+for n in range(N):  # n own strategy
+    # Implement games here?
