dbdab59d908c7f6e0ee00c02ed74dc74daf2034a
AxelEiman <axeleiman@gmail.com>
2021-12-07 11:40:37 +0100

    13.2 completed

--- Evolutionary Games/PD on lattice.py
+++ Evolutionary Games/PD on lattice.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 def play_rounds(n, m, nrounds):
     m_punishment = 0
     o_punishment = 0
+    m_prev = 0
+    o_prev = 0
 
     for r in range(nrounds):
-        m_play = make_choice(n, r)
-        o_play = make_choice(m, r)
+        m_play = make_choice(n, r, o_prev)
+        o_play = make_choice(m, r, m_prev)
 
         if m_play == 0 and o_play == 0:
             m_punishment += R
@@ -22,25 +24,27 @@ def play_rounds(n, m, nrounds):
         if m_play == 1 and o_play == 1:
             m_punishment += P
             o_punishment += P
-
+        m_prev = m_play
+        o_prev = o_play
     return m_punishment # , o_punishment
 
 
-def make_choice(strat, r):
-    if r+1 <= strat:
+def make_choice(strat, r, o_prev):
+    if r+1 <= strat and o_prev == 0:
         return 0
     else:
         return 1
 
 
-def compete(strats):
-    punishments = np.zeros(shape=strats.shape)
-    for r, line in enumerate(strats):
-        for c, strat in enumerate(line):
-            punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, 1, axis=0)[r, c], N)
-            punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, -1, axis=0)[r, c], N)
-            punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, 1, axis=1)[r, c], N)
-            punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, -1, axis=1)[r, c], N)
+def compete(strats, nRounds):
+    for round in range(nRounds):
+        punishments = np.zeros(shape=strats.shape)
+        for r, line in enumerate(strats):
+            for c, strat in enumerate(line):
+                punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, 1, axis=0)[r, c], N)
+                punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, -1, axis=0)[r, c], N)
+                punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, 1, axis=1)[r, c], N)
+                punishments[r, c] += play_rounds(strats[r, c], np.roll(strats, -1, axis=1)[r, c], N)
     return punishments
 
 
@@ -56,54 +60,74 @@ def update_strats(s, p):
     su = np.roll(s, 1, axis=0)
     sd = np.roll(s, -1, axis=0)
 
+    s_new = np.zeros(shape=s.shape)
     for r, line in enumerate(p):
         for c, punishment in enumerate(p):
             strats = [s[r, c], sl[r, c], sr[r, c], su[r, c], sd[r, c]]
             punishments = [p[r, c], pl[r, c], pr[r, c], pu[r, c], pd[r, c]]
             id = np.argmin(punishments)
 
-            s[r, c] = strats[id]
+            s_new[r, c] = strats[id]
+    return s_new
+
+
+def run_games(s, nRounds, timesteps):
+    for timestep in range(timesteps):
+        p = compete(s, nRounds)
+        s = update_strats(s, p)
+
     return s
 
 
-N = 11      # Number of rounds
+N = 7      # Number of rounds
 T = 0       # Punishment for lone snitch
-R = 0.5     # Punishment if neither snitch
+R = 0.65     # Punishment if neither snitch
 P = 1       # Punishment if both snitch
 S = 1.5     # Punishment if snitched on
+timesteps = 20
 
 # 13.2
-L = 5
-strats = np.ones([L, L]) * (N - 0)
-strats[int(np.floor(L/2)), int(np.floor(L/2))] = 0
-print(f"strats: \n{strats}")
+L = 30
+strats = np.zeros([L, L]) * (N - 0)
+# strats[int(np.floor(L/2)), int(np.floor(L/2))] = 0
+# b)
+# strats[6,6], strats[12,12], strats[18, 18], strats[24,24] = 0,0,0,0
+# c)
+strats[int(np.floor(L/2)), int(np.floor(L/2))] = N
+strats[int(np.floor(L/2))+1, int(np.floor(L/2))] = N
+strats[int(np.floor(L/2))-1, int(np.floor(L/2))] = N
+strats[int(np.floor(L/2)), int(np.floor(L/2))+1] = N
+strats[int(np.floor(L/2)), int(np.floor(L/2))-1] = N
 
-punishments = compete(strats)
 
-print(f"Punishments: \n{punishments}")
 
-new_strats = update_strats(strats, punishments)
-print(f"New strats: \n{new_strats}")
+# print(f"strats: \n{strats}")
+# punishments = compete(strats)
+# print(f"Punishments: \n{punishments}")
+# new_strats = update_strats(strats, punishments)
+# print(f"New strats: \n{new_strats}")
+
+strats_after = run_games(strats, N, timesteps)
 
 # Plotting
 fig = plt.figure()
-ax1 = fig.add_subplot(221)
+ax1 = fig.add_subplot(121)
 ax1.set_title("strategies")
 im1 = ax1.imshow(strats)
 ax1.figure.colorbar(im1)
 
-ax2 = fig.add_subplot(222)
-im2 = ax2.imshow(punishments)
-ax2.set_title("Punishments")
-ax2.figure.colorbar(im2)
+# ax2 = fig.add_subplot(222)
+# im2 = ax2.imshow(punishments)
+# ax2.set_title("Punishments")
+# ax2.figure.colorbar(im2)
 
-ax3 = fig.add_subplot(223)
-im3 = ax3.imshow(new_strats)
+ax3 = fig.add_subplot(122)
+im3 = ax3.imshow(strats_after)
 ax3.set_title("New strats")
 ax3.figure.colorbar(im3)
 
 ax1.axis('off')
-ax2.axis('off')
+# ax2.axis('off')
 ax3.axis('off')
 plt.show()
 
